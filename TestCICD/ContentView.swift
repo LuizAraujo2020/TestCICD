2547816877d50f71f5019e5db98718dcf5c59b6b
LuizAraujo2020 <luizcarlos_bsb2006@hotmail.com>
2022-07-19 14:44:55 -0300

    Initial Commit

---
+++ TestCICD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestCICD
+//
+//  Created by Luiz Araujo on 19/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestCICD/TestCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCICDApp.swift
+//  TestCICD
+//
+//  Created by Luiz Araujo on 19/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TestCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
